21912313f427b4edab9a66b661788433a8a7093a
Lance Jabr <lancejabr@gmail.com>
2019-02-16 12:43:37 -0800

    Initial Commit

---
+++ Swift++/Swift__.h
@@ -0,0 +1,19 @@
+//
+//  Swift__.h
+//  Swift++
+//
+//  Created by Lance Jabr on 2/16/19.
+//  Copyright © 2019 Lance Jabr. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Swift__.
+FOUNDATION_EXPORT double Swift__VersionNumber;
+
+//! Project version string for Swift__.
+FOUNDATION_EXPORT const unsigned char Swift__VersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Swift__/PublicHeader.h>
+
+
